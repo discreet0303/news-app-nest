c65ee7e9e595657f8c75e9a39537da90863ed116
MVP-Husan <chehsuan.chang@mvpfastlane.com>
2021-03-22 16:09:46 +0800

    import news service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ORM_CONFIG } from './config/ormconfig';
 
+import { NewsModule } from './news/news.module';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(ORM_CONFIG),
+    NewsModule
   ],
   controllers: [AppController],
   providers: [AppService],
