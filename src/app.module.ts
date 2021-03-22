71b47642ed70dee2ded143eb739580ac09807f4a
MVP-Husan <chehsuan.chang@mvpfastlane.com>
2021-03-22 16:07:43 +0800

    import typeorm and add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ORM_CONFIG } from './config/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ORM_CONFIG),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
